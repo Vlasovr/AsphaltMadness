91061c6e16fd9521a603d8c3b4e2ba1723eb503e
Роман Власов <78149386+Vlasovr@users.noreply.github.com>
2023-10-10 20:45:33 +0300

    fixed bugs with buttons in diff iOS versions

---
+++ AsphaltMadness/Resources/Data.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var listOfColors: [String: UIColor] = ["blue": .blue, "brown": .brown, "cyan": .cyan, "green": .green, "orange": .orange, "purple": .purple, "red": .red, "yellow": .yellow, "systemPink": .systemPink]
+
+var dangerObjectsList: [String] = ["redDangerObject",
+                                   "blackDangerObject",
+                                   "blueDangerObject",
+                                   "greenDangerObject",
+                                   "lightBlueDangerObject",
+                                   "policeDangerObject",
+                                   "taxiDangerObject",
+                                   "whiteDangerObject"]
---
+++ AsphaltMadness/Views/CircularImageView.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//MARK: - Setup avatar
+
+class CircularImageView: UIImageView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.frame.size.height / 2
+        self.clipsToBounds = true
+    }
+}
